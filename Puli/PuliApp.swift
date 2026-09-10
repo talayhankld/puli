d1872b8f92673bf2f137cbcce8b79dfa89671733
Talayhan Kalender <talayhan53@gmail.com>
2026-09-10 23:44:18 +0300

    Initial Commit

---
+++ Puli/PuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuliApp.swift
+//  Puli
+//
+//  Created by Talayhan on 10.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct PuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
